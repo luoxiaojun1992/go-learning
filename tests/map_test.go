55e8c5fedc66e92712ebeb7a522342af749dd323
luoxiaojun <luoxiaojun1992@sina.cn>
2022-01-13 23:34:58 +0800

    update tests/map_test.go

--- tests/map_test.go
+++ tests/map_test.go
@@ -9,3 +9,14 @@ func TestEmptyMap(t *testing.T) {
 	map2 := make(map[int]int)
 	t.Log(map2 == nil)
 }
+
+func TestMapComparison(t *testing.T) {
+	//error mismatched key value type
+	//map1 := make(map[int]int)
+	//map2 := make(map[int]string)
+	//map3 := make(map[string]int)
+	//
+	//map1 == map2
+	//map1 == map3
+	//map2 == map3
+}

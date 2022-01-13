1cf31f109f4275e4dceeb8b5dd1ce162fc4f10a8
luoxiaojun <luoxiaojun1992@sina.cn>
2022-01-13 23:01:56 +0800

    feat: update tests/chan_test.go

--- tests/chan_test.go
+++ tests/chan_test.go
@@ -2,6 +2,14 @@ package tests
 
 import "testing"
 
+func TestEmptyChannel(t *testing.T) {
+	var ch1 chan int
+	t.Log(ch1 == nil)
+
+	ch2 := make(chan int)
+	t.Log(ch2 == nil)
+}
+
 func TestChannelComparison(t *testing.T) {
 	var ch1 chan int
 	var ch2 chan int
